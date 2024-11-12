91a36a6d088ddb2b7a05c5543d390034cbc5e99f
Lenovo <Lenovo@DESKTOP-10R3SAU>
2024-11-11 19:20:11 -0600

    Agregar archivos de proyecto.

---
+++ Sistemas me esta matando/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace Sistemas_me_esta_matando
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SubirUp = new System.Windows.Forms.Button();
+            this.BajarDown = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // SubirUp
+            // 
+            this.SubirUp.Location = new System.Drawing.Point(33, 37);
+            this.SubirUp.Name = "SubirUp";
+            this.SubirUp.Size = new System.Drawing.Size(75, 23);
+            this.SubirUp.TabIndex = 0;
+            this.SubirUp.Text = "Sube";
+            this.SubirUp.UseVisualStyleBackColor = true;
+            this.SubirUp.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // BajarDown
+            // 
+            this.BajarDown.Location = new System.Drawing.Point(33, 79);
+            this.BajarDown.Name = "BajarDown";
+            this.BajarDown.Size = new System.Drawing.Size(75, 23);
+            this.BajarDown.TabIndex = 1;
+            this.BajarDown.Text = "Baja";
+            this.BajarDown.UseVisualStyleBackColor = true;
+            this.BajarDown.Click += new System.EventHandler(this.BajarDown_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(234, 47);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Piso";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(302, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Humo";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(231, 81);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(302, 81);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(35, 13);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(372, 47);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Peso";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label6.Location = new System.Drawing.Point(372, 81);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 13);
+            this.label6.TabIndex = 7;
+            this.label6.Text = "label6";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BajarDown);
+            this.Controls.Add(this.SubirUp);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button SubirUp;
+        private System.Windows.Forms.Button BajarDown;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Sistemas me esta matando/Form1.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Sistemas_me_esta_matando
+{
+    public partial class Form1 : Form
+    {
+        delegate void setTextDelegate(string val);
+        public SerialPort ArduinoPort { get; }
+        public Form1()
+        {
+            InitializeComponent();
+
+            ArduinoPort = new SerialPort("COM3", 9600);
+            ArduinoPort.DataReceived += new SerialDataReceivedEventHandler(DataRecievedHandler);
+
+            try
+            {
+                ArduinoPort.Open(); // Abre el puerto
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al abrir el puerto: " + ex.Message);
+            }
+        }
+
+        void DataRecievedHandler(object sender, SerialDataReceivedEventArgs e) // se recive el dato
+        {
+            string dato = ArduinoPort.ReadLine();
+            string[] valores = dato.Split(','); // Divide los datos con comas
+            // Verificar si hay 3 valores
+            if (valores.Length == 3)
+            {
+                string temp = valores[0];
+                string peso = valores[0];
+                string humo = valores[0];
+
+                ActualizarLabels(temp, peso, humo); // Se manda a escribir el dato en el label
+            }            
+        }
+
+        void ActualizarLabels(string temp, string peso, string humo)
+        {
+            if (label3.InvokeRequired || label4.InvokeRequired || label6.InvokeRequired)
+            {
+                label3.Invoke(new Action(() =>
+                {
+                    label3.Text = $"Temperatura: {temp} °C";
+                    label4.Text = $"Peso: {peso} kg";
+                    label6.Text = $"Humo: {humo} %";
+                }));
+            }
+            else
+            {
+                label3.Text = $"Temperatura: {temp} °C";
+                label4.Text = $"Peso: {peso} kg";
+                label6.Text = $"Humo: {humo} %";
+            }
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("A");
+        }
+
+        private void BajarDown_Click(object sender, EventArgs e)
+        {
+            ArduinoPort.Write("B");
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Cierra el puerto serial al cerrar el formulario
+            if (ArduinoPort.IsOpen)
+            {
+                ArduinoPort.Close();
+            }
+        }
+    } 
+}
